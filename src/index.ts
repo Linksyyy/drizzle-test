56eb1ba25f6d3354db1236c8a65c0d59b26eb264
Linksyyy <enzovidero@gmail.com>
2025-06-30 06:46:39 -0300

    🙏

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { usersTable } from './db/schema.ts'
-import {eq } from 'drizzle-orm';
-import { faker } from '@faker-js/faker'
+import { drizzle } from "drizzle-orm/node-postgres";
+import { usersTable } from "./db/schema.ts";
+import { eq } from "drizzle-orm";
+import { faker } from "@faker-js/faker";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
 async function main() {
-  const randomNumber = () => Math.round(Math.random()*30);
+  const randomNumber = () => Math.round(Math.random() * 100);
   const randomName = faker.person.fullName();
   const randomEmail = faker.internet.email();
 
-    const user = usersTable.bu$inferInsert = {
+  const user = {
     name: randomName,
     age: randomNumber(),
-    email: randomEmail
+    email: randomEmail,
   };
 
   await db.insert(usersTable).values(user);
-  console.log('user inserido')
-}
-for (let i = 0; i < 100; i++) {
-  await main()
 }
 
-console.log(await db.select().from(usersTable)
-)
+for (let i = 0; i < 5; i++) {
+  await main();
+}
+
+const allUsers = await db
+  .select({ id: usersTable.id, name: usersTable.name })
+  .from(usersTable);
+
+console.log(allUsers)
